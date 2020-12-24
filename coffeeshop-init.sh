33c7ca6ddea663fa0948479f4df34085d86e8647
Erdem Caglayan <erdemcaglayan_1@hotmail.com>
2020-12-24 22:46:33 +0300

    Refactored coffee shop init script.

--- coffeeshop-init.sh
+++ coffeeshop-init.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-start_container() {
-	DB_STAT="$( docker container inspect -f '{{.State.Running}}' $1 )"
-	if [[ "$DB_STAT" ]]; then
-        	 echo "$1 is running already..."
-	else
-        	docker start coffeeshopdb
-	        echo "$1 started..."
-	fi	
-
+check_container() {
+	result="$( docker container inspect -f '{{.State.Running}}' $1 )"
+	[[ $result = true ]] && result=0 || result=1
+	return $result
 }
 
-start_container "coffeeshopdb"
-start_container "coffeeshopservices"
+start_container() {
+	check_container $1
+	if [[ "$?" -eq 0 ]]; then
+        	echo "$1 is running already..."
+	else
+        	docker start $1
+	        echo "$1 started..."
+	fi	
+}
+
+start_container coffeeshopdb
+check_container coffeeshopdb
+count=0
+while [[ "$?" -ne 0 && "$count++" -lt 5  ]]; do
+	echo "Waiting coffeeshopdb to start..."
+	sleep 5s
+	check_container coffeeshopdb
+done
+start_container coffeeshopservices
